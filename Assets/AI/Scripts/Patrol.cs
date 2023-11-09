acdc637138e10845cca01accdcf366ac8c7a35dc
Nizhon <martin.rengsjo@gmail.com>
2023-11-09 16:33:55 +0100

    AI can now walk between all point
    Added a acceptableradius for distance check between AI and waypoint.
    When within acceptableRadius the AI have reached it's desired location.

--- Assets/AI/Scripts/Patrol.cs
+++ Assets/AI/Scripts/Patrol.cs
@@ -8,9 +8,24 @@ public class Patrol : MonoBehaviour
     [SerializeField] Transform[] Waypoints;
     [SerializeField] private int _currWaypont;
     [SerializeField] NavMeshAgent navAgent;
+    [SerializeField] private float _acceptableRadius;
 
     void Update()
     {
-        navAgent.SetDestination(Waypoints[_currWaypont].position);
+        Transform wp = Waypoints[_currWaypont];
+
+        if ( Vector3.Distance( transform.position, wp.position ) < _acceptableRadius )
+            _currWaypont = ( _currWaypont + 1 ) % Waypoints.Length;
+        else
+        {
+            navAgent.SetDestination( wp.position );
+            Quaternion lookRotation = Quaternion.LookRotation( navAgent.velocity );
+            transform.rotation = Quaternion.RotateTowards( transform.rotation, lookRotation, navAgent.angularSpeed * Time.deltaTime );
+        }
+    }
+
+    void Wait()
+    {
+
     }
 }
