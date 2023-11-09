2bb11fca16924d555de7df91962a8ee63bd5e7e2
Nizhon <martin.rengsjo@gmail.com>
2023-11-09 17:59:42 +0100

    We now have wait timer and we can choose to loop or not

--- Assets/AI/Scripts/Patrol.cs
+++ Assets/AI/Scripts/Patrol.cs
@@ -5,27 +5,69 @@ using UnityEngine.AI;
 
 public class Patrol : MonoBehaviour
 {
-    [SerializeField] Transform[] Waypoints;
+    [SerializeField] NavMeshAgent _navAgent;
+    [SerializeField] Transform[] _waypoints;
+
+    [SerializeField] [Range( 0f, 10f )] private float _waitTimer;
+    private float _waitTime = 0f;
     [SerializeField] private int _currWaypont;
-    [SerializeField] NavMeshAgent navAgent;
+
     [SerializeField] private float _acceptableRadius;
+
+    [SerializeField] bool _waiting;
+    [SerializeField] bool _loop;
 
     void Update()
     {
-        Transform wp = Waypoints[_currWaypont];
-
-        if ( Vector3.Distance( transform.position, wp.position ) < _acceptableRadius )
-            _currWaypont = ( _currWaypont + 1 ) % Waypoints.Length;
-        else
-        {
-            navAgent.SetDestination( wp.position );
-            Quaternion lookRotation = Quaternion.LookRotation( navAgent.velocity );
-            transform.rotation = Quaternion.RotateTowards( transform.rotation, lookRotation, navAgent.angularSpeed * Time.deltaTime );
-        }
+        Wait();
+        PathPatrol();
     }
 
     void Wait()
     {
+        if( _waiting )
+        {
+            _waitTime += Time.deltaTime;
 
+            if ( _waitTime < _waitTimer )
+                return;
+
+            _waiting = false;
+        }
+    }
+
+    void PathPatrol()
+    {
+        Transform wp = _waypoints[_currWaypont];
+
+        if ( Vector3.Distance( transform.position, wp.position ) < _acceptableRadius )
+        {
+            _waitTime = 0f;
+            _waiting = true;
+
+            if ( _loop )
+            {
+                if ( _currWaypont > _waypoints.Length )
+                    _currWaypont--;
+
+                _currWaypont = ( _currWaypont + 1 ) % _waypoints.Length;
+            } 
+            else
+            {
+                if ( _currWaypont >= _waypoints.Length - 1 )
+                    return;
+
+                _currWaypont++;
+            }
+        }
+        else
+        {
+            if( !_waiting )
+            {
+                _navAgent.SetDestination( wp.position );
+                Quaternion lookRotation = Quaternion.LookRotation( _navAgent.velocity );
+                transform.rotation = Quaternion.RotateTowards( transform.rotation, lookRotation, _navAgent.angularSpeed * Time.deltaTime );
+            }
+        }
     }
 }
